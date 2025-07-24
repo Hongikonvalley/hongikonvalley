1b86ecd54f00d8b1f215be9f6dfdea013ab0ad95
최우혁 <whc9999@naver.com>
2025-07-24 10:11:57 +0900

    [Feat] 프로젝트 파일 업로드

---
+++ src/main/java/com/example/egerdon/EgerdonApplication.java
@@ -0,0 +1,13 @@
+package com.example.egerdon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgerdonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgerdonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/egerdon/EgerdonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egerdon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgerdonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
